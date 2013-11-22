216340564dcb97a1b71dc28b60f498af7e1756c7
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-11-22 19:05:08 +0100

    added two scripts that set up a bunch of environment variables. One script script is for running on the test data, while the other is for running on the full data set.

--- set-idl-enram-test-environment.sh
+++ set-idl-enram-test-environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # execute this script in a terminal by:
-# . set-idl-enram-environment.sh
+# . set-idl-enram-test-environment.sh
 # (don't omit the dot at the beginning)
 # then stat idl or idlde from that same terminal with
 # idl &
@@ -20,9 +20,9 @@ export IDL_PATH=\+$IDL_DIR/lib:+$IDL_DIR/external/lib:+/home/daisycutter/enram/s
 # define where the different ENRAM directories are:
 export IDL_ENRAM_VOL2BIRDLIB='/home/daisycutter/enram/src/vol2bird/vol2bird.so'
 export IDL_ENRAM_SRC_IO='/home/daisycutter/enram/src/io/'
-export IDL_ENRAM_RAW_DATA='/home/daisycutter/enram/data/raw/'
-export IDL_ENRAM_ODIM_DATA='/home/daisycutter/enram/data/harmonized/odim/'
-export IDL_ENRAM_BIRD_DATA='/home/daisycutter/enram/data/harmonized/bird/'
-export IDL_ENRAM_CLUTTERMAPS='/home/daisycutter/enram/data/harmonized/cluttermaps/'
-export IDL_ENRAM_STATISTICS='/home/daisycutter/enram/data/harmonized/statistics/'
-export IDL_ENRAM_CLUTTER_SENSITIVTY='/home/daisycutter/enram/data/harmonized/clutter-sensitivity/'
+export IDL_ENRAM_RAW_DATA='/home/daisycutter/enram/testdata/raw/'
+export IDL_ENRAM_ODIM_DATA='/home/daisycutter/enram/testdata/harmonized/odim/'
+export IDL_ENRAM_BIRD_DATA='/home/daisycutter/enram/testdata/harmonized/bird/'
+export IDL_ENRAM_CLUTTERMAPS='/home/daisycutter/enram/testdata/harmonized/cluttermaps/'
+export IDL_ENRAM_STATISTICS='/home/daisycutter/enram/testdata/harmonized/statistics/'
+export IDL_ENRAM_CLUTTER_SENSITIVTY='/home/daisycutter/enram/testdata/harmonized/clutter-sensitivity/'
