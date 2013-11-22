88c8676c1dc332836db1185bf12c6a3c009470a7
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-11-22 15:12:01 +0100

    added a bash script that sets up the IDL environment

---
+++ set-idl-enram-environment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# set IDL paths
+. /home/daisycutter/exelis/idl/bin/idl_setup.bash
+
+# define where the IDL binary is located:
+export IDL_DIR=~/exelis/idl82;
+
+# define in which directories IDL will search for functions, scripts, and so on:
+export IDL_PATH=\+$IDL_DIR/lib:+$IDL_DIR/external/lib:+/home/daisycutter/enram/src;
+
+# define where the different ENRAM directories are:
+export IDL_ENRAM_VOL2BIRDLIB='/home/daisycutter/enram/src/vol2bird/vol2bird.so'
+export IDL_ENRAM_SRC_IO='/home/daisycutter/enram/src/io/'
+export IDL_ENRAM_RAW_DATA='/home/daisycutter/enram/data/raw/'
+export IDL_ENRAM_ODIM_DATA='/home/daisycutter/enram/data/harmonized/odim/'
+export IDL_ENRAM_BIRD_DATA='/home/daisycutter/enram/data/harmonized/bird/'
+export IDL_ENRAM_CLUTTERMAPS='/home/daisycutter/enram/data/harmonized/cluttermaps/'
+export IDL_ENRAM_STATISTICS='/home/daisycutter/enram/data/harmonized/statistics/'
