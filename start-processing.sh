b0a3246359e69e9f89189cf18e929e3535caa45e
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-11-23 15:39:33 +0100

    the main script now redirects its feedback to log/stderr.log and log/stdout.log files for later reference.

--- start-processing.sh
+++ start-processing.sh
@@ -3,22 +3,38 @@
 # start this script from the terminal by (make sure to enter it exactly):
 # . ./start-processing.sh
 
+outfile='log/stdout.log'
+errfile='log/stderr.log'
+
+
 echo "Do you wish to process the test data or the full data? (Enter 1 or 2)"
 select answer in "Test data only" "Full data"; do
     case $answer in
-        "Test data only" ) echo "Test data only"; . set-idl-enram-test-environment.sh; break;;
-        "Full data" ) echo "Full data"; . set-idl-enram-environment.sh; break;;
+        "Test data only" ) echo "Test data only" 1>$outfile 2>$errfile; . set-idl-enram-test-environment.sh; break;;
+        "Full data" ) echo "Full data" 1>$outfile 2>$errfile; . set-idl-enram-environment.sh; break;;
     esac
 done
 
+echo "Started processing based on $answer..."
+echo "IDL_ENRAM_VOL2BIRDLIB='$IDL_ENRAM_VOL2BIRDLIB'" 1>>$outfile
+echo "IDL_ENRAM_SRC_IO='$IDL_ENRAM_SRC_IO'" 1>>$outfile
+echo "IDL_ENRAM_RAW_DATA='$IDL_ENRAM_RAW_DATA'" 1>>$outfile
+echo "IDL_ENRAM_ODIM_DATA='$IDL_ENRAM_ODIM_DATA'" 1>>$outfile
+echo "IDL_ENRAM_BIRD_DATA='$IDL_ENRAM_BIRD_DATA'" 1>>$outfile
+echo "IDL_ENRAM_CLUTTERMAPS='$IDL_ENRAM_CLUTTERMAPS'" 1>>$outfile
+echo "IDL_ENRAM_STATISTICS='$IDL_ENRAM_STATISTICS'" 1>>$outfile
+echo "IDL_ENRAM_CLUTTER_SENSITIVITY='$IDL_ENRAM_CLUTTER_SENSITIVITY'" 1>>$outfile
+echo "IDL_ENRAM_VISUALIZATION_OUTPUT='$IDL_ENRAM_VISUALIZATION_OUTPUT'" 1>>$outfile
 
 
-idl -e ".r histogram.pro"
+idl -e ".r histogram.pro" 1>>$outfile 2>>$errfile
 
-idl -e ".r cluttermap.pro"
+idl -e ".r cluttermap.pro" 1>>$outfile 2>>$errfile 
 
-idl -e ".r clutter_sensitivity.pro"
+idl -e ".r clutter_sensitivity.pro" 1>>$outfile 2>>$errfile
+ 
+idl -e ".r flysafe2.pro" 1>>$outfile 2>>$errfile
 
-idl -e ".r flysafe2.pro"
 
 
+echo "Started processing based on $answer...Done"
