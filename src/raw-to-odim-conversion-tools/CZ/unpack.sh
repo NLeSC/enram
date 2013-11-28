706b07b215f13cc06f53b2910cb549cc2da2dd10
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-11-28 18:03:39 +0100

    edited unpack.sh (not finished yet)

--- src/raw-to-odim-conversion-tools/CZ/unpack.sh
+++ src/raw-to-odim-conversion-tools/CZ/unpack.sh
@@ -1,49 +1,53 @@
 #!/bin/bash
 
+# next line needed for ShellEd Bash debugger for Eclipse:
+. _DEBUG.sh
+
+# script requires that ENRAM_* environment variables have been set
+
 #Czech Republic 
 #RUN for id='T_PAHZ60' and destination=CZ_brd
 #AND     id='T_PAHZ50' and destination=CZ_ska
 
+: ${ENRAM_RAW_DATA:?"script requires ENRAM_RAW_DATA to have been set."}
+: ${ENRAM_CONVERTED_DATA:?"script requires ENRAM_CONVERTED_DATA to have been set."}
+: ${ENRAM_TMP:?"script requires ENRAM_TMP to have been set."}
 
-#move and unpack to:
-destination='/nobackup_1/users/graafdem/DATA/FLYSAFE2/odim/CZ_'
-#REMOVE original file?
-#delete=1
+# set source and destination
+source=$ENRAM_RAW_DATA'CZ/czech*'
+destination=$ENRAM_CONVERTED_DATA'CZ_'
 
-#
-#
-#
-
-files=`ls -1 czech*` 
+files=`ls -1 $source`
+ 
 thisdir=`pwd`
 
 echo $thisdir
 
 
-#initialise counter
+#initialize counter
 i=`expr 0`
 for file in $files
 do
 
-  name=${file%.tar.gz}		#strip file extention
-  date=${name:6}		#get only the date indication, use for path
+  name=${file%.tar.gz}      #strip file extension
+  date=${name:6}            #get only the date indication, use for path
   $stationname
   echo -en '\r' `date`": Unpacking $date"
   #
-  tar xfz $file
+  tar --directory $ENRAM_TMP -xzf $file
   #
-  #FiND the different system files and redistribute them
+  #Find the different system files and redistribute them
   cd 'hdf5/'$date
-  
-  allfiles=`ls -1 *.hdf`
-  number_of_files=`ls -1 *.hdf | wc -l`
+  #
+  allfiles=`ls -1 $source*.hdf`
+  number_of_files=`ls -1 $source*.hdf | wc -l`
   #
   #PROCESS all files
   for ifile in $allfiles
   do 
     #
     name=${ifile%.hdf}
-    stationcode=${name:6:2}	
+    stationcode=${name:6:2} 
     #
     #REDISTRIBUTE according to station name
     if [ $stationcode == 60 ] 
@@ -62,24 +66,12 @@ do
     mv $ifile  $thisdestination
     #
     let i=i+1
-  #
-  #if [ $i == 350 ]
-  #then 
-  # exit 0
-  #fi  
-  #
   done   
   #
-  #
   cd $thisdir
-  # 
-#  if [ $delete ] 
-#  then 
-#    echo -en '\r' `date`": Deleting $file"
-#    rm -f $file
-#  fi
+  #
 done 
-#
+
 #remove unpack directory
 rm -fr 'hdf5'
 
