e11fbde19f1be461e030208c6e397eb30462bb72
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-12-13 11:38:09 +0100

    added a readme document that explains how to install virtualbox on windows, and how to set up a virtual machine

---
+++ doc/readme/tex/compile-latex.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+str='readme'
+
+
+latex -output-directory=./../out -interaction=nonstopmode $str.tex
+cd ./../out
+bibtex $str.aux
+makeindex -o $str.ind $str.idx
+cd ./../tex
+latex -output-directory=./../out -interaction=nonstopmode $str.tex
+latex -output-directory=./../out -interaction=nonstopmode $str.tex
+dvips -o ./../out/$str.ps ./../out/$str.dvi
+ps2pdf ./../out/$str.ps ./../out/$str.pdf
+
+rm ./../out/$str.aux
+rm ./../out/$str.bbl
+rm ./../out/$str.blg
+rm ./../out/$str.dvi
+rm ./../out/$str.idx
+rm ./../out/$str.ilg
+rm ./../out/$str.ind
+rm ./../out/$str.log
+rm ./../out/$str.out
+rm ./../out/$str.ps
+#rm ./../out/$str.toc
