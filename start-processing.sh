39394799370ec68801d4ada81354123193fde127
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-01-29 13:05:20 +0100

    reorganized start-processing.sh for better readability

--- start-processing.sh
+++ start-processing.sh
@@ -10,14 +10,24 @@ errfile='log/stderr.txt'
 echo "Do you wish to process the test data or the full data? (Enter 1 or 2)"
 select answer in "Test data only" "Full data"; do
     case $answer in
-        "Test data only" ) echo "Test data only" 1>$outfile 2>$errfile; . set-idl-enram-test-environment.sh; break;;
-        "Full data" ) echo "Full data" 1>$outfile 2>$errfile; . set-idl-enram-environment.sh; break;;
+        "Test data only" ) 
+           echo "Test data only" 1>$outfile 2>$errfile
+           . ./set-idl-enram-test-environment.sh
+           break
+           ;;
+        "Full data" )
+           echo "Full data" 1>$outfile 2>$errfile
+           . ./set-idl-enram-environment.sh
+           break
+           ;;
     esac
 done
 
 echo "Started processing based on $answer..."
 echo "(processing logs are located in '$outfile' and '$errfile')"
-echo "You can use the browser to inspect those files while the ENRAM scripts are running"
+echo "You can use the browser to inspect those files while the ENRAM scripts are running, but leave this window open."
+
+
 echo "IDL_ENRAM_VOL2BIRDLIB='$IDL_ENRAM_VOL2BIRDLIB'" 1>>$outfile
 echo "IDL_ENRAM_SRC_IO='$IDL_ENRAM_SRC_IO'" 1>>$outfile
 echo "IDL_ENRAM_RAW_DATA='$IDL_ENRAM_RAW_DATA'" 1>>$outfile
@@ -33,10 +43,8 @@ idl -e ".r histogram.pro" 1>>$outfile 2>>$errfile
 
 idl -e ".r cluttermap.pro" 1>>$outfile 2>>$errfile
 
-idl -e ".r clutter_sensitivity.pro" 1>>$outfile 2>>$errfile
+#idl -e ".r clutter_sensitivity.pro" 1>>$outfile 2>>$errfile
 
 idl -e ".r flysafe2.pro" 1>>$outfile 2>>$errfile
-
-
 
 echo "Started processing based on $answer...Done"
