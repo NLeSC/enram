5d154bf46a5d06b41e42dbd69aa2322a42cc66b8
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-02-04 16:39:10 +0100

    some more edits relating to redirection of the standard output and standard error to the screen as well as to log files

--- start-processing.sh
+++ start-processing.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 # execute this script in a terminal by:
-# . set-idl-enram-test-environment.sh
+# . ./start-processing.sh
 # (don't omit the dot at the beginning)
-# then stat idl or idlde from that same terminal with
-# idl &
-# or
-# idlde &
+# then answer the questions in the terminal
 
-# location of the enram directory
+
+
+
+
+# location of the enram directory:
 enramroot='/home/wbouten/enram/'
+
+
+
+
+
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 #             NO NEED TO EDIT ANYTHING BELOW THIS LINE                #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-
-
 
 echo "Do you want an interactive session or a batch processing session? (Enter 1 or 2)"
 select answer1 in "Interactive" "Batch"; do
@@ -85,38 +88,46 @@ else
     errfile=${enramroot}'log/stderr.txt'
 
 
-	echo `date`": Started processing based on "$answer2"..." | tee $outfile
-	echo "(processing logs are located in "$outfile" and "$errfile")"
-	echo "You can use the browser to inspect those files while the ENRAM scripts are running, but leave this window open."
-	
-	echo "IDL_ENRAM_VOL2BIRDLIB="${IDL_ENRAM_VOL2BIRDLIB} 1>>$outfile
-	echo "IDL_ENRAM_SRC_IO="${IDL_ENRAM_SRC_IO} 1>>$outfile
-	echo "IDL_ENRAM_RAW_DATA="${IDL_ENRAM_RAW_DATA} 1>>$outfile
-	echo "IDL_ENRAM_ODIM_DATA="${IDL_ENRAM_ODIM_DATA} 1>>$outfile
-	echo "IDL_ENRAM_BIRD_DATA="${IDL_ENRAM_BIRD_DATA} 1>>$outfile
-	echo "IDL_ENRAM_CLUTTERMAPS="${IDL_ENRAM_CLUTTERMAPS} 1>>$outfile
-	echo "IDL_ENRAM_STATISTICS="${IDL_ENRAM_STATISTICS} 1>>$outfile
-	echo "IDL_ENRAM_CLUTTER_SENSITIVITY="${IDL_ENRAM_CLUTTER_SENSITIVITY} 1>>$outfile
-	echo "IDL_ENRAM_VISUALIZATION_OUTPUT="${IDL_ENRAM_VISUALIZATION_OUTPUT} 1>>$outfile
-	echo "IDL_ENRAM_TEMPLATES="${IDL_ENRAM_TEMPLATES} 1>>$outfile
-	
-	idl -e ".r histogram.pro" 1>>$outfile 2>>$errfile
-	
-	idl -e ".r cluttermap.pro" 1>>$outfile 2>>$errfile
-	
-	idl -e ".r flysafe2.pro" 1>>$outfile 2>>$errfile
-	
-	idl -e ".r clutter_sensitivity.pro" 1>>$outfile 2>>$errfile
-	
-	idl -e ".r make_colored_donut_maps.pro" 1>>$outfile 2>>$errfile
-	
-	idl -e ".r make_ppi_time_space_bird_rain.pro" 1>>$outfile 2>>$errfile
-	
-	idl -e ".r multi_ppi.pro" 1>>$outfile 2>>$errfile
-	
-	idl -e ".r make_bird_density_profiles.pro" 1>>$outfile 2>>$errfile
-	
-	echo `date`": Started processing based on "$answer2"...Done" | tee 1>>$outfile
+    echo -n "" > ${outfile}
+    echo -n "" > ${errfile}
+    
+    str=`date`": Started processing based on "$answer2"..." 
+    echo ${str}
+    echo ${str}>>${outfile}
+    
+    echo "(processing logs are located in "${outfile}" and "${errfile}")"
+    echo "You can use the browser to inspect those files while the ENRAM scripts are running, but leave this window open."
+    
+    echo "IDL_ENRAM_VOL2BIRDLIB="${IDL_ENRAM_VOL2BIRDLIB} 1>>${outfile}
+    echo "IDL_ENRAM_SRC_IO="${IDL_ENRAM_SRC_IO} 1>>${outfile}
+    echo "IDL_ENRAM_RAW_DATA="${IDL_ENRAM_RAW_DATA} 1>>${outfile}
+    echo "IDL_ENRAM_ODIM_DATA="${IDL_ENRAM_ODIM_DATA} 1>>${outfile}
+    echo "IDL_ENRAM_BIRD_DATA="${IDL_ENRAM_BIRD_DATA} 1>>${outfile}
+    echo "IDL_ENRAM_CLUTTERMAPS="${IDL_ENRAM_CLUTTERMAPS} 1>>${outfile}
+    echo "IDL_ENRAM_STATISTICS="${IDL_ENRAM_STATISTICS} 1>>${outfile}
+    echo "IDL_ENRAM_CLUTTER_SENSITIVITY="${IDL_ENRAM_CLUTTER_SENSITIVITY} 1>>${outfile}
+    echo "IDL_ENRAM_VISUALIZATION_OUTPUT="${IDL_ENRAM_VISUALIZATION_OUTPUT} 1>>${outfile}
+    echo "IDL_ENRAM_TEMPLATES="${IDL_ENRAM_TEMPLATES} 1>>${outfile}
+    
+    idl -e ".r histogram.pro" 1>>${outfile} 2>>${errfile}
+    
+    idl -e ".r cluttermap.pro" 1>>${outfile} 2>>${errfile}
+    
+    idl -e ".r flysafe2.pro" 1>>${outfile} 2>>${errfile}
+    
+    idl -e ".r clutter_sensitivity.pro" 1>>${outfile} 2>>${errfile}
+    
+    idl -e ".r make_colored_donut_maps.pro" 1>>${outfile} 2>>${errfile}
+    
+    idl -e ".r make_ppi_time_space_bird_rain.pro" 1>>${outfile} 2>>${errfile}
+    
+    idl -e ".r multi_ppi.pro" 1>>${outfile} 2>>${errfile}
+    
+    idl -e ".r make_bird_density_profiles.pro" 1>>${outfile} 2>>${errfile}
+    
+    str=`date`": Started processing based on "$answer2"...Done"
+    echo ${str}
+    echo ${str}>>${outfile}
 
 fi
 
