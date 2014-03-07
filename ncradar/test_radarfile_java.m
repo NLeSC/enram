f12b76ea011948a52952ba47c56e2cb0c928d717
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-03-07 18:10:04 +0100

    test file now plots DBZH, VRAD, and WRAD as imagesc and as polar plot for comparison

--- ncradar/test_radarfile_java.m
+++ ncradar/test_radarfile_java.m
@@ -10,30 +10,50 @@ import nl.esciencecenter.ncradar.*
 
 javaaddpath('./lib/netcdfAll-4.3.jar')
 
-rs = RadarScan('/home/daisycutter/tmp','T_PAGZ60_C_OKPR_20110815000447.hdf');
-
-theScanData = rs.getScanData();
-
-nBins = rs.getNumberOfBins();
-nRays = rs.getNumberOfRays();
-
-
-
-
-% profile on
-tic
-
-% Maak een MATLAB figuurtje
-figure
-imagesc(theScanData)
+iScan = 4;
 
 figure
-plotppi(rs)
+
+rsDBZH = RadarScan('/home/daisycutter/tmp/enram-20140306/enram/testdata/harmonized/odim/CZ_brd/20110815/','T_PAGZ60_C_OKPR_20110815000447.hdf',iScan);
+theScanData = rsDBZH.getScanData();
+
+subplot(2,3,1)
+imagesc(theScanData,[-128,127])
+title([char(rsDBZH.getDatasetName()),' // ',char(rsDBZH.getScanType())])
+
+subplot(2,3,4)
+plotppi(rsDBZH)
 axis image
-axis off
+axis off 
+drawnow
+clear rsDBZH
 
-% print naar CSV in een format dat CartoDB kan lezen:
-rf.printAsWKTToCSV()
 
-toc
-% profile viewer
+rsVRAD = RadarScan('/home/daisycutter/tmp/enram-20140306/enram/testdata/harmonized/odim/CZ_brd/20110815/','T_PAHZ60_C_OKPR_20110815000447.hdf',iScan);
+theScanData = rsVRAD.getScanData();
+
+subplot(2,3,2)
+imagesc(theScanData,[-128,127])
+title([char(rsVRAD.getDatasetName()),' // ',char(rsVRAD.getScanType())])
+
+subplot(2,3,5)
+plotppi(rsVRAD)
+axis image
+axis off 
+drawnow
+clear rsVRAD
+
+rsWRAD = RadarScan('/home/daisycutter/tmp/enram-20140306/enram/testdata/harmonized/odim/CZ_brd/20110815/','T_PAIZ60_C_OKPR_20110815000447.hdf',iScan);
+theScanData = rsWRAD.getScanData();
+
+subplot(2,3,3)
+imagesc(theScanData,[-128,127])
+title([char(rsWRAD.getDatasetName()),' // ',char(rsWRAD.getScanType())])
+
+subplot(2,3,6)
+plotppi(rsWRAD)
+axis image
+axis off 
+drawnow
+
+clear WRAD
