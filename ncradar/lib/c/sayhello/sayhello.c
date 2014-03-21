59d21fed5f56b3c60c10b9227e9b34242e1194e7
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-03-21 13:41:24 +0100

    changed the layout of the project for better compliance with conventions

--- ncradar/lib/c/sayhello/sayhello.c
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-#include <stdio.h>
-#include "nl_esciencecenter_ncradar_RadarScan.h"
-
-// Implementation of native method sayHello() of RadarScan class
-JNIEXPORT void JNICALL Java_RadarScan_sayHello(JNIEnv *env, jobject thisObj) {
-   printf("Hello World from C :p  \n");
-   return;
-}
