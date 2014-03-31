c82a8a2f18d3729efef49ac8363d9865c54424eb
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-03-31 18:09:55 +0200

    added the C code example libsayhello, the libvol2bird example, and the java netcdf libraries to the newly created lib/ directory

---
+++ ncradar/lib/sayhello/libsayhello.c
@@ -0,0 +1,9 @@
+#include <jni.h>
+#include <stdio.h>
+#include "nl_esciencecenter_ncradar_RadarScan.h"
+
+// Implementation of native method sayHello() of RadarScan class
+JNIEXPORT void JNICALL Java_nl_esciencecenter_ncradar_RadarScan_sayHello(JNIEnv *env, jobject thisObj) {
+   printf("Hello World from C :p  \n");
+   return;
+}
