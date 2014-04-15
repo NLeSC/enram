8201e13e53611b69a25d273baf5fee358ec43bba
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-04-15 18:35:56 +0200

    added the native method findcell

--- ncradar/src/nl/esciencecenter/ncradar/JNIMethodsVol2Bird.java
+++ ncradar/src/nl/esciencecenter/ncradar/JNIMethodsVol2Bird.java
@@ -4,12 +4,19 @@ public class JNIMethodsVol2Bird {
 
     static {
         System.loadLibrary("calctexture");
+        System.loadLibrary("findcells");
         }
          
     // Declare native methods
     final native static int[] calcTexture(int tMissing, int tnAzim, int tnRange, double tOffset, double tScale,
-                     int[] vImage, int vMissing, int vnAzim, int vnRange, double vOffset, double vScale,
-                     int[] zImage, int zMissing, int znAzim, int znRange, double zOffset, double zScale,
-                     int nRangLocal, int nAzimLocal, int nCountMin, int textype);
-	
+                            int[] vImage, int vMissing, int vnAzim, int vnRange, double vOffset, double vScale,
+                            int[] zImage, int zMissing, int znAzim, int znRange, double zOffset, double zScale,
+                            int nRangLocal, int nAzimLocal, int nCountMin, int textype);
+    
+    final native static int[] findCells(int[] cellImage, 
+            int[] texImage, int texnRang, int texnAzim, double texOffset, double texScale, int texMissing, double texThresMin, 
+            int[] rhoImage, int rhonRang, int rhonAzim, double rhoOffset, double rhoScale, int rhoMissing, double rhoThresMin,
+            int[] zdrImage, int zdrnRang, int zdrnAzim, double zdrOffset, double zdrScale, int zdrMissing, double zdrThresMin, 
+            double dbzmin, double rcellmax, int sign, double texrScale);
+    
 }
