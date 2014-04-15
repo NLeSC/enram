c56d07d3bff3fd0ef3a8e142f5d5558acfe12a1e
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-04-15 18:32:05 +0200

    removed some test files related to JNI --no longer needed

--- ncradar/lib/sayhello/libsayhello.c
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-#include <stdio.h>
-#include "nl_esciencecenter_ncradar_JNIMethodsVol2Bird.h"
-
-// Implementation of native method sayHello() of JNIMethodsVol2Bird class
-JNIEXPORT void JNICALL Java_nl_esciencecenter_ncradar_JNIMethodsVol2Bird_sayHello(JNIEnv *env, jobject thisObj) {
-   printf("Hello World from C :p  \n");
-   return;
-}
