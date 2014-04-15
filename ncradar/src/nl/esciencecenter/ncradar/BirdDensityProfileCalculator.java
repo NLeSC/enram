7bd95c3e860eddc168dbc26c1637592869b4644f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-04-15 11:05:21 +0200

    added a proof of concept of reading a DBZ and a VRAD scan into two RadarScan objects, and combining the information to calculate int[] texture --seems to work

--- ncradar/src/nl/esciencecenter/ncradar/BirdDensityProfileCalculator.java
+++ ncradar/src/nl/esciencecenter/ncradar/BirdDensityProfileCalculator.java
@@ -7,6 +7,7 @@ public class BirdDensityProfileCalculator extends JNIMethodsVol2Bird {
 
     private RadarScan rsz;
     private RadarScan rsv;
+    
 
     public BirdDensityProfileCalculator(RadarScan rsz, RadarScan rsv) throws IOException {
 
@@ -24,24 +25,14 @@ public class BirdDensityProfileCalculator extends JNIMethodsVol2Bird {
         BirdDensityProfileCalculator birdprofile = new BirdDensityProfileCalculator(rsz,rsv);
 
 
-        int tMissing = 23;
-        int tnAzim = 5;
-        int tnRange = 6;
-        double tOffset = 0.00;
-        double tScale = 0.001;
-        
-        int[] vImage = new int[] {60,61,62,63,64,65,66,67,68,69,
-                                  70,71,72,73,74, 5,76,77,78,79,
-                                  80,81,82,83,84,85,86,87,88,89};
+        int[] vImage = rsv.convertScanData();
         int vMissing = rsv.getMissingValue();
         int vnAzim = (int) rsv.getNumberOfAzimuthBins();
         int vnRange = (int) rsv.getNumberOfRangeBins();
         double vOffset = rsv.getOffset();
         double vScale = rsv.getGain();
 
-        int[] zImage = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                                  10, 1,12,13,14,15,16,17,18,19,
-                                  20,21,22,23,24,25,26,27,28,29};
+        int[] zImage = rsz.convertScanData();
         int zMissing = rsz.getMissingValue();
         int znAzim = (int) rsz.getNumberOfAzimuthBins();
         int znRange = (int) rsz.getNumberOfRangeBins();
@@ -53,6 +44,13 @@ public class BirdDensityProfileCalculator extends JNIMethodsVol2Bird {
         int nCountMin = 4;
         int textype = 2; // (TEXSTDEV)
 
+        int tMissing = 255;
+        int tnAzim = vnAzim;
+        int tnRange = vnRange;
+        double tOffset = 0.00;
+        double tScale = 1;
+
+
         int[] texture = calcTexture(tMissing,tnAzim,tnRange,tOffset,tScale,
                                vImage,vMissing,vnAzim,vnRange,vOffset,vScale,
                                zImage,zMissing,znAzim,znRange,zOffset,zScale,
@@ -60,11 +58,12 @@ public class BirdDensityProfileCalculator extends JNIMethodsVol2Bird {
 
         int nElems = texture.length;
 
-        for (int iElem=-0;iElem<nElems;iElem++){
-            System.out.println("(Java) iElem "+iElem+" = "+texture[iElem]);
+        for (int iElem=0;iElem<nElems;iElem++){
+            System.err.println("(Java) iElem "+iElem+" = "+texture[iElem]);
         }
 
     }
+    
 
 
 
