48f3afeb816b70a748bd030be3e22ac4276a59d5
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-04-28 17:07:12 +0200

    deleted some commented-out code

--- ncradar/src/nl/esciencecenter/ncradar/JNIMethodsVol2Bird.java
+++ ncradar/src/nl/esciencecenter/ncradar/JNIMethodsVol2Bird.java
@@ -8,11 +8,7 @@ public class JNIMethodsVol2Bird {
         System.loadLibrary("fringecells");
         System.loadLibrary("updatemap");
         System.loadLibrary("sortcells");
-        System.loadLibrary("findcells");
-
-        
-
-
+        //System.loadLibrary("findcells");
         }
          
 //    // Declare native methods
@@ -20,13 +16,6 @@ public class JNIMethodsVol2Bird {
 //                            int[] vImage, int vMissing, int vnAzim, int vnRange, double vOffset, double vScale,
 //                            int[] zImage, int zMissing, int znAzim, int znRange, double zOffset, double zScale,
 //                            int nRangLocal, int nAzimLocal, int nCountMin, int textype);
-//    
-//    final native static int[] findCells(int[] cellImage, 
-//            int[] texImage, int texnRang, int texnAzim, double texOffset, double texScale, int texMissing, double texThresMin, 
-//            int[] rhoImage, int rhonRang, int rhonAzim, double rhoOffset, double rhoScale, int rhoMissing, double rhoThresMin,
-//            int[] zdrImage, int zdrnRang, int zdrnAzim, double zdrOffset, double zdrScale, int zdrMissing, double zdrThresMin, 
-//            double dbzmin, double rcellmax, int sign, double texrScale);
-//
 //    
     // new interface method starts here:
     
