84146367a3c072888acca3aff2f97b66403463e7
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-04-29 11:50:12 +0200

    cleaned up BirdDensityProfileCalculator

--- ncradar/src/nl/esciencecenter/ncradar/BirdDensityProfileCalculator.java
+++ ncradar/src/nl/esciencecenter/ncradar/BirdDensityProfileCalculator.java
@@ -18,92 +18,14 @@ public class BirdDensityProfileCalculator extends JNIMethodsVol2Bird {
     
     public static void main(String[] args) throws IOException{
 
-        // test 
-        findCells(int[] cellImage,
-                char[] texImage,
-                char[] rhoImage,
-                char[] zdrImage,
-                float dbzThresMin,
-                int texMissing,
-                int texnAzim,
-                int texnRang,
-                float texValueOffset,
-                float texRangeScale,
-                float texValueScale,
-                float texThresMin,
-                int rhoMissing,
-                int rhonAzim,
-                int rhonRang,
-                float rhoValueOffset,
-                float rhoValueScale,
-                float rhoThresMin,
-                int zdrMissing,
-                int zdrnAzim,
-                int zdrnRang,
-                float zdrValueOffset,
-                float zdrValueScale,
-                float zdrThresMin,
-                float rCellMax,
-                char sign);
-        
-        
-        
-        
-        
-        
-        
-//        int[] cellImage = new int[] {2,3,0,1,2,3,4,5};
-//        int nRang = 4;
-//        int nAzim = 2;
-//        float aScale = 0.4f;
-//        float rScale = 0.9f;
-//        float fringe = 2;
-//        fringecells(cellImage, nRang, nAzim, aScale, rScale, fringe);
-        
+        RadarScan rsz = new RadarScan("/home/wbouten/tmp","T_PAGZ60_C_OKPR_20110815000447.hdf",3);
+        RadarScan rsv = new RadarScan("/home/wbouten/tmp","T_PAHZ60_C_OKPR_20110815000447.hdf",3);
 
-//        RadarScan rsz = new RadarScan("/home/wbouten/tmp","T_PAGZ60_C_OKPR_20110815000447.hdf",3);
-//        RadarScan rsv = new RadarScan("/home/wbouten/tmp","T_PAHZ60_C_OKPR_20110815000447.hdf",3);
-//
-//        BirdDensityProfileCalculator birdprofile = new BirdDensityProfileCalculator(rsz,rsv);
-//
-//
-//        int[] vImage = rsv.convertScanData();
-//        int vMissing = rsv.getMissingValue();
-//        int vnAzim = (int) rsv.getNumberOfAzimuthBins();
-//        int vnRange = (int) rsv.getNumberOfRangeBins();
-//        double vOffset = rsv.getOffset();
-//        double vScale = rsv.getGain();
-//
-//        int[] zImage = rsz.convertScanData();
-//        int zMissing = rsz.getMissingValue();
-//        int znAzim = (int) rsz.getNumberOfAzimuthBins();
-//        int znRange = (int) rsz.getNumberOfRangeBins();
-//        double zOffset = rsz.getOffset();
-//        double zScale = rsz.getGain();
-//
-//        int nRangLocal = 3;
-//        int nAzimLocal = 3;
-//        int nCountMin = 4;
-//        int textype = 2; // (TEXSTDEV)
-//
-//        int tMissing = 255;
-//        int tnAzim = vnAzim;
-//        int tnRange = vnRange;
-//        double tOffset = 0.00;
-//        double tScale = 1;
-//
-//
-//        int[] texture = calcTexture(tMissing,tnAzim,tnRange,tOffset,tScale,
-//                               vImage,vMissing,vnAzim,vnRange,vOffset,vScale,
-//                               zImage,zMissing,znAzim,znRange,zOffset,zScale,
-//                               nRangLocal,nAzimLocal,nCountMin,textype);
-//
-//        int nElems = texture.length;
-//
-//        for (int iElem=0;iElem<nElems;iElem++){
-//            System.err.println("(Java) iElem "+iElem+" = "+texture[iElem]);
-//        }
-//
+        BirdDensityProfileCalculator birdprofile = new BirdDensityProfileCalculator(rsz,rsv);
+        
+        
+        System.out.println("I have a BirdDensityProfileCalculator object.");
+        
     }
     
 
