43a2ae1c6d53ff2f7d14acb8b15aff3bfd700bb1
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-04-29 18:21:08 +0200

    unit testing of ScanMeta class

--- ncradar/test/src/nl/esciencecenter/ncradar/TestScanMeta.java
+++ ncradar/test/src/nl/esciencecenter/ncradar/TestScanMeta.java
@@ -11,7 +11,7 @@ public class TestScanMeta {
     private int date = 942378;
     private int time = 2873423;
     private float radarHeight = 123.4f;
-    private float elevationAngle = 45.0f;
+    private float radarElevationAngle = 45.0f;
     private int numberOfRangeBins = 12;
     private int numberOfAzimuthBins = 15;
     private float binSizeRange = 67.8f;
@@ -31,7 +31,7 @@ public class TestScanMeta {
     @Before
     public void setUp() {
         
-        scanMeta = new ScanMeta(date, time, radarHeight, elevationAngle, 
+        scanMeta = new ScanMeta(date, time, radarHeight, radarElevationAngle, 
                 numberOfRangeBins, numberOfAzimuthBins, binSizeRange, 
                 binSizeAzimuth, iAzimFirstRay, valueOffset, 
                 valueScale, missingValueValue, pulseRepeatFrequencyHigh,
@@ -48,94 +48,175 @@ public class TestScanMeta {
     @Test
     public void testGetDate() {
         
-        int expected = 942378;
+        int expected = date;
         int actual = scanMeta.getDate();
         assertEquals(expected, actual);
     }
 
     @Test
     public void testGetTime() {
-        fail("Not yet implemented");
+        
+        int expected = time;
+        int actual = scanMeta.getTime();
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testGetRadarHeight() {
-        fail("Not yet implemented");
+
+        float expected = radarHeight;
+        float actual = scanMeta.getRadarHeight();
+        float delta = 0.00001f;
+        assertEquals(expected, actual, delta);
+
     }
 
     @Test
     public void testGetElevationAngle() {
-        fail("Not yet implemented");
+
+        float expected = radarElevationAngle;
+        float actual = scanMeta.getRadarElevationAngle();
+        float delta = 0.00001f;
+        assertEquals(expected, actual, delta);
+
     }
 
     @Test
     public void testGetNumberOfRangeBins() {
-        fail("Not yet implemented");
+
+        int expected = numberOfRangeBins;
+        int actual = scanMeta.getNumberOfRangeBins();
+        assertEquals(expected, actual);
+
     }
 
     @Test
     public void testGetNumberOfAzimuthBins() {
-        fail("Not yet implemented");
+
+        int expected = numberOfAzimuthBins;
+        int actual = scanMeta.getNumberOfAzimuthBins();
+        assertEquals(expected, actual);
+
     }
 
     @Test
     public void testGetBinSizeRange() {
-        fail("Not yet implemented");
+
+        float expected = binSizeRange;
+        float actual = scanMeta.getBinSizeRange();
+        float delta = 0.00001f;
+        assertEquals(expected, actual, delta);
+
     }
 
     @Test
     public void testGetBinSizeAzimuth() {
-        fail("Not yet implemented");
+
+        float expected = binSizeAzimuth;
+        float actual = scanMeta.getBinSizeAzimuth();
+        float delta = 0.00001f;
+        assertEquals(expected, actual, delta);
+
     }
 
     @Test
     public void testGetiAzimFirstRay() {
-        fail("Not yet implemented");
+        
+        int expected = iAzimFirstRay;
+        int actual = scanMeta.getiAzimFirstRay();
+        assertEquals(expected, actual);
+
     }
 
     @Test
     public void testGetValueOffset() {
-        fail("Not yet implemented");
+
+        float expected = valueOffset;
+        float actual = scanMeta.getValueOffset();
+        float delta = 0.00001f;
+        assertEquals(expected, actual, delta);
+
+        
     }
 
     @Test
     public void testGetValueScale() {
-        fail("Not yet implemented");
+
+        float expected = valueScale;
+        float actual = scanMeta.getValueScale();
+        float delta = 0.00001f;
+        assertEquals(expected, actual, delta);
+
     }
 
     @Test
     public void testGetMissingValueValue() {
-        fail("Not yet implemented");
+
+        int expected = missingValueValue;
+        int actual = scanMeta.getMissingValueValue();
+        assertEquals(expected, actual);
+
     }
 
     @Test
     public void testGetPulseRepeatFrequencyHigh() {
-        fail("Not yet implemented");
+
+        float expected = pulseRepeatFrequencyHigh;
+        float actual = scanMeta.getPulseRepeatFrequencyHigh();
+        float delta = 0.00001f;
+        assertEquals(expected, actual, delta);
+
     }
 
     @Test
     public void testGetPulseRepeatFrequencyLow() {
-        fail("Not yet implemented");
+        
+        float expected = pulseRepeatFrequencyLow;
+        float actual = scanMeta.getPulseRepeatFrequencyLow();
+        float delta = 0.00001f;
+        assertEquals(expected, actual, delta);
+
     }
 
     @Test
     public void testGetPulseLength() {
-        fail("Not yet implemented");
+
+        float expected = pulseLength;
+        float actual = scanMeta.getPulseLength();
+        float delta = 0.00001f;
+        assertEquals(expected, actual, delta);
+
     }
 
     @Test
     public void testGetRadarConstant() {
-        fail("Not yet implemented");
+
+        float expected = radarConstant;
+        float actual = scanMeta.getRadarConstant();
+        float delta = 0.00001f;
+        assertEquals(expected, actual, delta);
+
     }
 
     @Test
     public void testGetNominalMaxTransmissionPower() {
-        fail("Not yet implemented");
+
+        float expected = nominalMaxTransmissionPower;
+        float actual = scanMeta.getNominalMaxTransmissionPower();
+        float delta = 0.00001f;
+        assertEquals(expected, actual, delta);
+
     }
 
     @Test
     public void testGetRadialVelocityAntenna() {
-        fail("Not yet implemented");
+        
+        float expected = radialVelocityAntenna;
+        float actual = scanMeta.getRadialVelocityAntenna();
+        float delta = 0.00001f;
+        assertEquals(expected, actual, delta);
+
+        
     }
 
 }
