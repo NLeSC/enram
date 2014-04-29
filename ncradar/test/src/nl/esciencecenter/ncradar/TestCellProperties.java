c99de6677c6548ee0e13ea9b8907deaa78d3d453
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-04-29 11:49:09 +0200

    added a test class for testing the class CellProperties.java

---
+++ ncradar/test/src/nl/esciencecenter/ncradar/TestCellProperties.java
@@ -0,0 +1,14 @@
+package nl.esciencecenter.ncradar;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCellProperties {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
