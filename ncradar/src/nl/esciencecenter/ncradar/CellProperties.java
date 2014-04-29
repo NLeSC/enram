3beeae62b9d00bb76416b5f566a83cc622e2cbda
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-04-29 11:52:08 +0200

    removed the setter methods from this class. Added final modifier to class properties (all class properties can now only be set in the constructor and that's it --protection against accidentally changing stuff)

--- ncradar/src/nl/esciencecenter/ncradar/CellProperties.java
+++ ncradar/src/nl/esciencecenter/ncradar/CellProperties.java
@@ -2,16 +2,16 @@ package nl.esciencecenter.ncradar;
 
 public class CellProperties {
     
-    private int[] iRangOfMax;
-    private int[] iAzimOfMax; 
-    private float[] dbzAvg; 
-    private float[] texAvg; 
-    private float[] cv; 
-    private float[] area; 
-    private float[] clutterArea; 
-    private float[] max; 
-    private int[] index; 
-    private char[] drop; 
+    private final int[] iRangOfMax;
+    private final int[] iAzimOfMax; 
+    private final float[] dbzAvg; 
+    private final float[] texAvg; 
+    private final float[] cv; 
+    private final float[] area; 
+    private final float[] clutterArea; 
+    private final float[] max; 
+    private final int[] index; 
+    private final char[] drop; 
     
     public CellProperties(int[] iRangOfMax, int[] iAzimOfMax, float[] dbzAvg, float[] texAvg, float[] cv, 
                           float[] area, float[] clutterArea, float[] max, int[] index, char[] drop) {
@@ -33,83 +33,41 @@ public class CellProperties {
         return iRangOfMax;
     }
 
-    public void setiRangOfMax(int[] iRangOfMax) {
-        this.iRangOfMax = iRangOfMax;
-    }
-
     public int[] getiAzimOfMax() {
         return iAzimOfMax;
-    }
-
-    public void setiAzimOfMax(int[] iAzimOfMax) {
-        this.iAzimOfMax = iAzimOfMax;
     }
 
     public float[] getDbzAvg() {
         return dbzAvg;
     }
 
-    public void setDbzAvg(float[] dbzAvg) {
-        this.dbzAvg = dbzAvg;
-    }
-
     public float[] getTexAvg() {
         return texAvg;
-    }
-
-    public void setTexAvg(float[] texAvg) {
-        this.texAvg = texAvg;
     }
 
     public float[] getCv() {
         return cv;
     }
 
-    public void setCv(float[] cv) {
-        this.cv = cv;
-    }
-
     public float[] getArea() {
         return area;
-    }
-
-    public void setArea(float[] area) {
-        this.area = area;
     }
 
     public float[] getClutterArea() {
         return clutterArea;
     }
 
-    public void setClutterArea(float[] clutterArea) {
-        this.clutterArea = clutterArea;
-    }
-
     public float[] getMax() {
         return max;
-    }
-
-    public void setMax(float[] max) {
-        this.max = max;
     }
 
     public int[] getIndex() {
         return index;
     }
 
-    public void setIndex(int[] index) {
-        this.index = index;
-    }
-
     public char[] getDrop() {
         return drop;
     }
 
-    public void setDrop(char[] drop) {
-        this.drop = drop;
-    }
 
-    
-
-    
 }
