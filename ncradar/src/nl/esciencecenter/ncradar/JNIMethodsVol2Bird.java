c3447ab8d5071d49831118b60db516e51db96be9
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-05-16 18:48:30 +0200

    Removed a couiple of methods (those are now in BirdDensityProfile)--- this class should just contain the interfaces to C

--- ncradar/src/nl/esciencecenter/ncradar/JNIMethodsVol2Bird.java
+++ ncradar/src/nl/esciencecenter/ncradar/JNIMethodsVol2Bird.java
@@ -22,8 +22,6 @@ public class JNIMethodsVol2Bird {
             float[] cellPropDbzMax, int[] cellPropIndex, char[] cellPropDrop,
             int nCells, int nGlobal, int minCellArea);
 
-    private byte[][] texture;
-
     static {
 
         try {
@@ -78,77 +76,14 @@ public class JNIMethodsVol2Bird {
 
 
 
-    protected static final native int findCells(int[] cellImage,
-            char[] texImage, char[] rhoImage, char[] zdrImage,
-            float dbzThresMin, int texMissing, int texnAzim, int texnRang,
-            float texValueOffset, float texRangeScale, float texValueScale,
-            float texThresMin, int rhoMissing, int rhonAzim, int rhonRang,
-            float rhoValueOffset, float rhoValueScale, float rhoThresMin,
-            int zdrMissing, int zdrnAzim, int zdrnRang, float zdrValueOffset,
-            float zdrValueScale, float zdrThresMin, float rCellMax, char sign);
-
-
-
-    protected CellProperties sortCells(CellProperties cellPropIn, int method)
-            throws Exception {
-
-        int[] iRangOfMax = cellPropIn.getAlliRangOfMax();
-        int[] iAzimOfMax = cellPropIn.getAlliAzimOfMax();
-        float[] dbzAvg = cellPropIn.getAllDbzAvg();
-        float[] texAvg = cellPropIn.getAllTexAvg();
-        float[] cv = cellPropIn.getAllCv();
-        float[] area = cellPropIn.getAllArea();
-        float[] clutterArea = cellPropIn.getAllClutterArea();
-        float[] dbzMax = cellPropIn.getAllDbzMax();
-        int[] index = cellPropIn.getAllIndex();
-        char[] drop = cellPropIn.getAllDrop();
-
-        int nCells = cellPropIn.getnCells();
-
-        sortCells(iRangOfMax, iAzimOfMax, dbzAvg, texAvg, cv, area,
-                clutterArea, dbzMax, index, drop, nCells, method);
-
-        CellProperties cellPropOut = cellPropIn.clone();
-
-        cellPropOut.copyCellPropertiesFrom(iRangOfMax, iAzimOfMax, dbzAvg,
-                texAvg, cv, area, clutterArea, dbzMax, index, drop);
-
-        return cellPropOut;
-
-    }
-
-
-
-    protected int[] updateMap(CellProperties cellProp, int nCells, int nGlobal,
-            int minCellArea) throws Exception {
-
-        int[] iRangOfMax = new int[nCells];
-        int[] iAzimOfMax = new int[nCells];
-        float[] dbzAvg = new float[nCells];
-        float[] texAvg = new float[nCells];
-        float[] cv = new float[nCells];
-        float[] area = new float[nCells];
-        float[] clutterArea = new float[nCells];
-        float[] dbzMax = new float[nCells];
-        int[] index = new int[nCells];
-        char[] drop = new char[nCells];
-
-        cellProp.copyCellPropertiesTo(iRangOfMax, iAzimOfMax, dbzAvg, texAvg,
-                cv, area, clutterArea, dbzMax, index, drop);
-
-        int nCellsValid;
-
-        int[] cellImage = new int[nGlobal];
-
-        nCellsValid = updateMap(cellImage, iRangOfMax, iAzimOfMax, dbzAvg,
-                texAvg, cv, area, clutterArea, dbzMax, index, drop, nCells,
-                nGlobal, minCellArea);
-
-        cellProp.copyCellPropertiesFrom(iRangOfMax, iAzimOfMax, dbzAvg, texAvg,
-                cv, area, clutterArea, dbzMax, index, drop);
-
-        return cellImage;
-
-    }
+    protected static final native int findCells(byte[] texImage,
+            byte[] rhoImage, byte[] zdrImage, int[] cellImage, int texMissing,
+            int texnAzim, int texnRang, float texValueOffset,
+            float texRangeScale, float texValueScale, float texThresMin,
+            int rhoMissing, int rhonAzim, int rhonRang, float rhoValueOffset,
+            float rhoValueScale, float rhoThresMin, int zdrMissing,
+            int zdrnAzim, int zdrnRang, float zdrValueOffset,
+            float zdrValueScale, float zdrThresMin, float reflThresMin,
+            float rCellMax, byte sign);
 
 }
