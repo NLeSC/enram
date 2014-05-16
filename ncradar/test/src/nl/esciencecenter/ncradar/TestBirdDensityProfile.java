a9767387fa41a3c58e8cb2a089cf946b0647c059
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-05-16 13:50:49 +0200

    added a unit test for the BirdDensityProfile class

---
+++ ncradar/test/src/nl/esciencecenter/ncradar/TestBirdDensityProfile.java
@@ -0,0 +1,56 @@
+package nl.esciencecenter.ncradar;
+
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestBirdDensityProfile {
+
+    private RadarScanJava radarScanReflectivity;
+    private RadarScanJava radarScanRadialVelocity;
+    private int iScan;
+    private BirdDensityProfile birdProfile;
+
+
+
+    @Before
+    public void setUp() throws Exception {
+
+        iScan = 3;
+        radarScanReflectivity = new RadarScanJava("/home/wbouten/tmp",
+                "T_PAGZ60_C_OKPR_20110815000447.hdf", iScan);
+        radarScanRadialVelocity = new RadarScanJava("/home/wbouten/tmp",
+                "T_PAHZ60_C_OKPR_20110815000447.hdf", iScan);
+
+        birdProfile = new BirdDensityProfile(radarScanReflectivity,
+                radarScanRadialVelocity);
+
+        System.out.println("I have a BirdDensityProfileCalculator object.");
+
+    }
+
+
+
+    @Test
+    public void testBirdDensityProfile() {
+
+        byte nRangNeighborhood = 3;
+        byte nAzimNeighborhood = 3;
+        byte nCountMin = 0;
+        byte texType = 1;
+        float texOffset = 0;
+        float texScale = 1;
+        byte[][] tex;
+
+        birdProfile.calcTexture(radarScanReflectivity, radarScanRadialVelocity,
+                nRangNeighborhood, nAzimNeighborhood, nCountMin, texType,
+                texOffset, texScale);
+
+        tex = birdProfile.getTexture();
+
+        fail("Not yet complete");
+
+    }
+
+}
