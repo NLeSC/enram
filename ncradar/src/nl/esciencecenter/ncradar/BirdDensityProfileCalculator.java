3ef506f5b32e964c77a57e977da7a8a6fd202c3c
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-05-16 11:25:45 +0200

    small changes

--- ncradar/src/nl/esciencecenter/ncradar/BirdDensityProfileCalculator.java
+++ ncradar/src/nl/esciencecenter/ncradar/BirdDensityProfileCalculator.java
@@ -5,19 +5,18 @@ import java.io.IOException;
 
 public class BirdDensityProfileCalculator extends JNIMethodsVol2Bird {
 
-    private RadarScanJava rsz;
-    private RadarScanJava rsv;
+    private RadarScanJava RadarScanReflectivity;
+    private RadarScanJava RadarScanRadialVelocity;
     
 
-    public BirdDensityProfileCalculator(RadarScanJava rsz, RadarScanJava rsv) throws IOException {
+    public BirdDensityProfileCalculator(RadarScanJava RadarScanReflectivity, RadarScanJava RadarScanRadialVelocity) throws IOException {
 
-        this.rsz = rsz;
-        this.rsv = rsv;
+        this.RadarScanReflectivity = RadarScanReflectivity;
+        this.RadarScanRadialVelocity = RadarScanRadialVelocity;
 
     }
     
     public static void main(String[] args) throws IOException{
-
         
         RadarScanJava rsz = new RadarScanJava("/home/wbouten/tmp","T_PAGZ60_C_OKPR_20110815000447.hdf",3);
         RadarScanJava rsv = new RadarScanJava("/home/wbouten/tmp","T_PAHZ60_C_OKPR_20110815000447.hdf",3);
@@ -25,10 +24,8 @@ public class BirdDensityProfileCalculator extends JNIMethodsVol2Bird {
         BirdDensityProfileCalculator birdprofile = new BirdDensityProfileCalculator(rsz,rsv);
         
         
+        
         System.out.println("I have a BirdDensityProfileCalculator object.");
-        
-        
-        
        
     }
     
