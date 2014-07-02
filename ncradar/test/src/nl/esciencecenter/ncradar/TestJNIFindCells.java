60e90f0e503735556b5e8b7430d2b085f70eeb59
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-07-02 17:22:45 +0200

    updated unit test for testing the JNI coupling of findcells...does not do much yet but at least it works.

--- ncradar/test/src/nl/esciencecenter/ncradar/TestJNIFindCells.java
+++ ncradar/test/src/nl/esciencecenter/ncradar/TestJNIFindCells.java
@@ -1,113 +1,65 @@
 package nl.esciencecenter.ncradar;
 
-import org.junit.Before;
 import org.junit.Test;
 
 
 public class TestJNIFindCells extends JNIMethodsVol2Bird {
 
-    private int[] texImage;
-    private int[] rhoImage;
-    private int[] zdrImage;
-    private int[] cellImage;
-    private int texMissing;
-    private int texnAzim;
-    private int texnRang;
-    private float texValueOffset;
-    private float texRangeScale;
-    private float texValueScale;
-    private float texThresMin;
-    private int rhoMissing;
-    private int rhonAzim;
-    private int rhonRang;
-    private float rhoValueOffset;
-    private float rhoValueScale;
-    private float rhoThresMin;
-    private int zdrMissing;
-    private int zdrnAzim;
-    private int zdrnRang;
-    private float zdrValueOffset;
-    private float zdrValueScale;
-    private float zdrThresMin;
-    private float reflThresMin;
-    private float rCellMax;
-    private int sign;
-    private int nAzim;
-    private int nRang;
-
-
-
-    @Before
-    public void setUp() throws Exception {
-
-        nAzim = 5;
-        nRang = 4;
-
-        int missing = 255;
-
-        texImage = new int[] { 0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0 };
-        rhoImage = new int[] { 0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0 };
-        zdrImage = new int[] { 0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0 };
-        cellImage = new int[] { 0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0 };
-        texMissing = missing;
-        texnAzim = nAzim;
-        texnRang = nRang;
-        texValueOffset = 0.0f;
-        texRangeScale = 1.0f;
-        texValueScale = 1.0f;
-        texThresMin = 0.0f;
-        rhoMissing = missing;
-        rhonAzim = nAzim;
-        rhonRang = nRang;
-        rhoValueOffset = 0.0f;
-        rhoValueScale = 1.0f;
-        rhoThresMin = 0.0f;
-        zdrMissing = missing;
-        zdrnAzim = nAzim;
-        zdrnRang = nRang;
-        zdrValueOffset = 0.0f;
-        zdrValueScale = 1.0f;
-        zdrThresMin = 0.0f;
-        reflThresMin = 0.0f;
-        rCellMax = 0;
-        sign = 0;
-
-    }
-
-
-
     @Test
     public void testNativeFindCells() throws Exception {
 
-        // TODO this call sometimes hangs...ask somebody
+        int nAzim = 5;
+        int nRang = 7;
+
+        int missing = 255;
+
+        int[] texImage = new int[] { 0, 54, 54, 45, 0, 0, 0,
+                0, 45, 45, 45, 0, 0, 0,
+                0, 54, 54, 54, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, };
+        int[] rhoImage = null;
+        int[] zdrImage = null;
+        int[] cellImage = new int[] { 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0 };
+        int texMissing = missing;
+        int texnAzim = nAzim;
+        int texnRang = nRang;
+        float texValueOffset = 0.0f;
+        float texValueScale = 1.0f;
+        float texRangeScale = 10.0f;
+        float texThresMin = 130.0f;
+        int rhoMissing = missing;
+        int rhonAzim = nAzim;
+        int rhonRang = nRang;
+        float rhoValueOffset = 0.0f;
+        float rhoValueScale = 1.0f;
+        float rhoThresMin = 0.0f;
+        int zdrMissing = missing;
+        int zdrnAzim = nAzim;
+        int zdrnRang = nRang;
+        float zdrValueOffset = 0.0f;
+        float zdrValueScale = 1.0f;
+        float zdrThresMin = 0.0f;
+        float dbzThresMin = 0.0f;
+        int rCellMax = (int) (nRang * texRangeScale);
+        int sign = 1;
 
         int nCells = findCells(texImage, rhoImage, zdrImage, cellImage,
-                texMissing, texnAzim, texnRang, texValueOffset, texRangeScale, texValueScale, texThresMin,
+                texMissing, texnAzim, texnRang, texValueOffset, texValueScale, texRangeScale, texThresMin,
                 rhoMissing, rhonAzim, rhonRang, rhoValueOffset, rhoValueScale, rhoThresMin,
                 zdrMissing, zdrnAzim, zdrnRang, zdrValueOffset, zdrValueScale, zdrThresMin,
-                reflThresMin, rCellMax, sign);
+                dbzThresMin, rCellMax, sign);
 
-        int[][] actual = reshapeTo2D(cellImage, nAzim, nRang);
+        int[][] cellImageActual = reshapeTo2D(cellImage, nAzim, nRang);
 
-        int[][] expected = {};
+        int[][] cellImageExpected = { { nCells } };
 
-        // FIXME test nCells
+        // TODO test nCells
+        // TODO test cellImage result
 
     }
 
