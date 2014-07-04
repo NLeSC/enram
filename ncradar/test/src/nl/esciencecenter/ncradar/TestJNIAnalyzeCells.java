60255d38cbb11e6903685276adc65853de50102e
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-07-04 16:59:16 +0200

    completed the analyzecells unit test class. Now reading from a couple of text files so I also had to add a reader method.

--- ncradar/test/src/nl/esciencecenter/ncradar/TestJNIAnalyzeCells.java
+++ ncradar/test/src/nl/esciencecenter/ncradar/TestJNIAnalyzeCells.java
@@ -1,89 +1,42 @@
 package nl.esciencecenter.ncradar;
 
-import org.junit.Before;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import org.junit.Test;
 
 
 public class TestJNIAnalyzeCells extends JNIMethodsVol2Bird {
 
-    private int[] dbzImage;
-    private int[] vradImage;
-    private int[] texImage;
-    private int[] clutterImage;
-    private int[] cellImage;
-    private int dbznRang;
-    private int dbznAzim;
-    private float dbzElev;
-    private float dbzValueScale;
-    private float dbzValueOffset;
-    private float vradValueScale;
-    private float vradValueOffset;
-    private float clutterValueScale;
-    private float clutterValueOffset;
-    private float texValueScale;
-    private float texValueOffset;
-    private int nCells;
-    private int areaMin;
-    private float cellDbzMin;
-    private float cellStdDevMax;
-    private float cellClutterFraction;
-    private float vradMinValue;
-    private float dbzClutterMin;
-    private int cmFlagInt;
-    private int dualPolFlagInt;
-    private int verboseInt;
+    private int[] readDataFromFile(String filename) throws IOException {
 
+        List<Integer> arrList = new ArrayList<Integer>();
 
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
 
-    @Before
-    public void setUp() throws Exception {
+        String line;
 
-        texImage = new int[] { 0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0 };
-        rhoImage = new int[] { 0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0 };
-        zdrImage = new int[] { 0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0 };
-        cellImage = new int[] { 0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0 };
+        while ((line = reader.readLine()) != null) {
 
-        // TODO initialize all variables needed for calling analyzeCells
+            Scanner scanner = new Scanner(line);
+            while (scanner.hasNext()) {
+                int nextInt = scanner.nextInt();
+                arrList.add(nextInt);
+            }
 
-    }
+        }
+        reader.close();
 
+        int nElems = arrList.size();
+        int[] outputArray = new int[nElems];
+        for (int iElem = 0; iElem < nElems; iElem++) {
+            outputArray[iElem] = arrList.get(iElem).intValue();
+        }
 
-
-    @Test
-    public void testNativeAnalyzeCells() throws Exception {
-
-        int nCells = analyseCells(dbzImage, vradImage, texImage, clutterImage, cellImage,
-                dbznRang, dbznAzim,
-                dbzElev, dbzValueScale, dbzValueOffset,
-                vradValueScale, vradValueOffset,
-                clutterValueScale, clutterValueOffset,
-                texValueScale, texValueOffset,
-                nCells, areaMin, cellDbzMin, cellStdDevMax, cellClutterFraction,
-                vradMinValue, dbzClutterMin, cmFlagInt, dualPolFlagInt, verboseInt);
-
-        int nAzim = 5;
-        int nRang = 4;
-
-        int[][] actual = reshapeTo2D(cellImage, nAzim, nRang);
-
-        int[][] expected = {};
-
+        return outputArray;
     }
 
 
@@ -102,6 +55,59 @@ public class TestJNIAnalyzeCells extends JNIMethodsVol2Bird {
         }
 
         return outputArray;
+
+    }
+
+
+
+    @Test
+    public void testNativeAnalyzeCells() throws Exception {
+
+        int nAzim = 12;
+        int nRang = 11;
+
+        // FIXME this should not be an absolute address
+        int[] dbzImage = readDataFromFile("/home/wbouten/enram/ncradar/test/data/case1/testdata-12x11-pattern-dbz.txt");
+        int[] vradImage = readDataFromFile("/home/wbouten/enram/ncradar/test/data/case1/testdata-12x11-pattern-vrad.txt");
+        int[] texImage = readDataFromFile("/home/wbouten/enram/ncradar/test/data/case1/testdata-12x11-pattern-tex.txt");
+        int[] clutterImage = readDataFromFile("/home/wbouten/enram/ncradar/test/data/case1/testdata-12x11-pattern0.txt");
+        int[] cellImage = readDataFromFile("/home/wbouten/enram/ncradar/test/data/case1/testdata-12x11-pattern-cell.txt");
+        int dbznRang = nRang;
+        int dbznAzim = nAzim;
+        float dbzElev = 123.4f;
+        float dbzValueScale = 1.0f;
+        float dbzValueOffset = 0.0f;
+        float vradValueScale = 1.0f;
+        float vradValueOffset = 0.0f;
+        float clutterValueScale = 1.0f;
+        float clutterValueOffset = 0.0f;
+        float texValueScale = 1.0f;
+        float texValueOffset = 0.0f;
+        int nCells = 2;
+        int areaMin = 0;
+        float cellDbzMin = 0;
+        float cellStdDevMax = 0;
+        float cellClutterFraction = 0;
+        float vradMinValue = 0; //
+        float dbzClutterMin = 0;
+        int cmFlag = 0;
+        int dualPolFlag = 0;
+        int verbose = 0;
+
+        // TODO FIXME analyseCells still hangs sometimes due to bugs...
+
+        int nCellsValid = analyseCells(dbzImage, vradImage, texImage, clutterImage, cellImage,
+                dbznRang, dbznAzim,
+                dbzElev, dbzValueScale, dbzValueOffset,
+                vradValueScale, vradValueOffset,
+                clutterValueScale, clutterValueOffset,
+                texValueScale, texValueOffset,
+                nCells, areaMin, cellDbzMin, cellStdDevMax, cellClutterFraction,
+                vradMinValue, dbzClutterMin, cmFlag, dualPolFlag, verbose);
+
+        int[][] actual = reshapeTo2D(cellImage, nAzim, nRang);
+
+        int[][] expected = {};
 
     }
 
