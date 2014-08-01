299f515cf1d9c6320d2b4897ee24891da5aed366
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-08-01 19:12:07 +0200

    went through the integration test that calls calcTexture, findCells, analyzeCells, fringeCells, and classify, and changed my ad hoc parameter values to values that make more sense, using the values already stored in .reflectivity, .radialVelocity, or in .parameterValues. This is almost finished.

--- ncradar/test/src/nl/esciencecenter/ncradar/TestBirdDensityProfile.java
+++ ncradar/test/src/nl/esciencecenter/ncradar/TestBirdDensityProfile.java
@@ -51,105 +51,117 @@ public class TestBirdDensityProfile {
     public void testBirdDensityProfile() throws Exception {
 
         int missing = 255;
-        int nGlobal = nAzim * nRang;
-
-        // offsets
-        float dbzOffset = 0.0f;
-        float vradOffset = 0.0f;
-        float zdrOffset = 0.0f;
-        float rhoOffset = 0.0f;
-        float texOffset = 0;
-        float clutterOffset = 0.0f;
-
-        // scaling
-        float dbzScale = 1.0f;
-        float vradScale = 1.0f;
-        float rhoValueScale = 1.0f;
-        float zdrValueScale = 1.0f;
-        float texScale = 1.0f;
-        float clutterScale = 1.0f;
-
-        // missing
-        int dbzMissing = missing;
-        int rawReflMissing = missing;
-        int vradMissing = missing;
-        int rhoMissing = missing;
-        int zdrMissing = missing;
-
-        // nRang
-        int dbznRang = nRang;
-        int rhonRang = nRang;
-        int zdrnRang = nRang;
-
-        // nAzim
-        int dbznAzim = nAzim;
-        int rhonAzim = nAzim;
-        int zdrnAzim = nAzim;
-
-        // thresMin
-        float dbzThresMin = 130.0f;
-        float rhoThresMin = 0.0f;
-        float zdrThresMin = 0.0f;
-
-        // loose ends
-        final float DEG2RAD = (float) (2 * Math.PI) / 360;
         int nRangNeighborhood = this.parameterValues.getNTEXBINRANG();
         int nAzimNeighborhood = this.parameterValues.getNTEXBINAZIM();
         int nCountMin = this.parameterValues.getNTEXMIN();
-        float dbzRangeScale = 10.0f;
-        float dbzElev = 23.4f;
-        float dbzClutterMin = 0;
+        float texOffset = 0;
+        float texScale = (float) this.parameterValues.getSTDEVSCALE();
+        int vradMissing = missing;
 
-        int rCellMax = (int) (nRang * dbzRangeScale);
+        birdDensityProfile.calcTexture(nRangNeighborhood, nAzimNeighborhood, nCountMin,
+                texOffset, texScale, vradMissing);
+
+        int nGlobal = nAzim * nRang;
+        int[] zeros = new int[nGlobal];
+        int[] dbzImage = birdDensityProfile.getReflectivityRaw();
+        int[] rhoImage = null;
+        int[] zdrImage = null;
+        int[] cellImage = zeros.clone();
+        int dbzMissing = missing;
+        int dbznAzim = nAzim;
+        int dbznRang = nRang;
+        float dbzOffset = (float) this.reflectivity.getDataOffset();
+        float dbzScale = (float) this.reflectivity.getDataScale();
+        // FIXME dbzRangeScale seems to be stored as a number in [m], Adriaan
+        // assumes [km]?
+        float dbzRangeScale = (float) this.reflectivity.getRangeScale() / 1000;
+        float dbzThresMin = (float) this.parameterValues.getDBZRAIN() * dbzScale + dbzOffset;
+        int rhoMissing = missing;
+        int rhonAzim = nAzim;
+        int rhonRang = nRang;
+        float rhoOffset = 0.0f;
+        float rhoValueScale = 1.0f;
+        float rhoThresMin = (float) this.parameterValues.getRHOMIN();
+        int zdrMissing = missing;
+        int zdrnAzim = nAzim;
+        int zdrnRang = nRang;
+        float zdrOffset = 0.0f;
+        float zdrValueScale = 1.0f;
+        float zdrThresMin = (float) this.parameterValues.getZDRMIN();
+        // FIXME plus 5? magic number
+        int rCellMax = (int) (this.parameterValues.getRANGMAX() + 5);
         int sign = -1;
-        int nCellsValid;
 
-        int nCells;
-        int areaMin = 0;
-        float cellDbzMin = 0;
-        float cellStdDevMax = 0;
-        float cellClutterFraction = 0;
+        int nCells = BirdDensityProfile.findCells(dbzImage, rhoImage, zdrImage, cellImage,
+                dbzMissing, dbznAzim, dbznRang, dbzOffset, dbzScale, dbzRangeScale, dbzThresMin,
+                rhoMissing, rhonAzim, rhonRang, rhoOffset, rhoValueScale, rhoThresMin,
+                zdrMissing, zdrnAzim, zdrnRang, zdrOffset, zdrValueScale, zdrThresMin,
+                rCellMax, sign);
+
+        int[] vradImage = birdDensityProfile.getRadialVelocityRaw();
+        int[] texImage = birdDensityProfile.getTexture();
+        int[] clutterImage = zeros.clone();
+        float dbzElev = (float) this.reflectivity.getElevationAngle();
+        float vradScale = (float) this.radialVelocity.getDataScale();
+        float vradOffset = (float) this.radialVelocity.getDataOffset();
+        float clutterScale = 1.0f;
+        float clutterOffset = 0.0f;
+        int areaMin = 0; // AREACELL?
+        float cellDbzMin = 0; // DBZCELL?
+        float cellStdDevMax = 0; // STDEVCELL?
+        float cellClutterFraction = 0; // CLUTPERCCELL * 100 ?
         float vradMinValue = (float) parameterValues.getVRADMIN();
-
+        float dbzClutterMin = 0; // DBZCLUTTER?
         int cmFlag = 0;
         int dualPolFlag = 0;
         int verbose = 0;
-        float azimuthScale = 1.0f / DEG2RAD;
-        float rangeScale = 1.0f;
-        float fringeDist = 1.0f;
 
-        int[] zeros = new int[nGlobal];
-        int[] dbzImage = birdDensityProfile.getReflectivityRaw();
-        int[] vradImage = birdDensityProfile.getRadialVelocityRaw();
+        int nCellsValid = BirdDensityProfile.analyzeCells(dbzImage, vradImage,
+                texImage, clutterImage, cellImage,
+                dbznRang, dbznAzim, dbzElev, dbzScale, dbzOffset,
+                vradScale, vradOffset, clutterScale, clutterOffset,
+                texScale, texOffset, nCells, areaMin, cellDbzMin, cellStdDevMax,
+                cellClutterFraction, vradMinValue, dbzClutterMin, cmFlag,
+                dualPolFlag, verbose);
+
+        final float DEG2RAD = (float) (2 * Math.PI) / 360;
+        float azimuthScale = (360.0f / nAzim) * DEG2RAD;
+        // FIXME again, what are the units?
+        float rangeScale = (float) this.radialVelocity.getRangeScale() / 1000;
+        float fringeDist = (float) this.parameterValues.getEMASKMAX();
+
+        BirdDensityProfile.fringeCells(cellImage, nRang, nAzim, azimuthScale,
+                rangeScale, fringeDist);
+
+        float dbzHeig = (float) this.reflectivity.getRadarPositionHeight();
+
+        float dbzAzimScale = (360.0f / nAzim) * DEG2RAD;
+        int rawReflMissing = missing;
         int[] rawReflImage = zeros.clone();
-        int[] rhoImage = null;
-        int[] zdrImage = null;
-        int[] texImage = zeros.clone();
-        int[] cellImage = zeros.clone();
-        int[] clutterImage = zeros.clone();
-
-        float dbzHeig = 0.0f;
-        float dbzAzimScale = (float) (2 * Math.PI / 360);
-
+        // FIXME is zdata the profile information? (Then it should have multiple
+        // pseudo-columns)
         float[] zdata = new float[] { 0, 0, 0, 0 };
-        int[] nzdata = new int[] { 0, 0, 0 };
-        float rangeMin = 0.0f;
-        float rangeMax = 10000.0f;
+        float rangeMin = (float) this.parameterValues.getRANGMIN();
+        float rangeMax = (float) this.parameterValues.getRANGMAX();
         float HLAYER = (float) this.parameterValues.getHLAYER();
-        float XOFFSET = 0.0f;
-        float XSCALE = 0.0f;
-        float XMEAN = 0.0f;
-        float height = 0.0f;
-        float azimMin = 0.0f;
-        float azimMax = (float) (2 * Math.PI);
-        float vradMin = 0.0f;
-        float dbzClutter = 100.0f;
-        float dbzMin = 0.0f;
-        float dBZx = 0.0f;
+        float XOFFSET = 0.0f; // FIXME unclear what this does
+        float XSCALE = 1.0f; // FIXME unclear what this does
+        float XMEAN = 0.0f; // FIXME unclear what this does
+        float height = 2000.0f;
+        float azimMin = (float) this.parameterValues.getAZIMMIN();
+        float azimMax = (float) this.parameterValues.getAZIMMAX();
+        float vradMin = (float) this.parameterValues.getVRADMIN();
+        float dbzClutter = (float) this.parameterValues.getDBZCLUTTER();
+        float dbzMin = (float) this.parameterValues.getDBZMIN();
+        float dBZx = (float) this.parameterValues.getDBZMAX();
         float DBZNOISE = (float) this.parameterValues.getDBZNOISE();
         int NGAPMIN = this.parameterValues.getNGAPMIN();
         int NGAPBIN = this.parameterValues.getNGAPBIN();
         int NDBZMIN = this.parameterValues.getNDBZMIN();
+
+        // TODO this is how far I got with mapping this.parameterValues to the
+        // variables used here
+
         int layer = 0;
         int np = 0;
         int nPointsPtr = 0;
@@ -161,41 +173,23 @@ public class TestBirdDensityProfile {
         int rawReflFlagInt = 0;
         int xflagInt = 0;
 
-        birdDensityProfile.calcTexture(nRangNeighborhood, nAzimNeighborhood, nCountMin,
-                texOffset, texScale, vradMissing);
+        // FIXME it seems zdata should be bigger, probably NDATA * llayer or
+        // something to accommodate all the stuff that classify() wants to put
+        // in it.
 
-        texImage = birdDensityProfile.getTexture();
-
-        nCells = BirdDensityProfile.findCells(dbzImage, rhoImage, zdrImage, cellImage,
-                dbzMissing, dbznAzim, dbznRang, dbzOffset, dbzScale, dbzRangeScale, dbzThresMin,
-                rhoMissing, rhonAzim, rhonRang, rhoOffset, rhoValueScale, rhoThresMin,
-                zdrMissing, zdrnAzim, zdrnRang, zdrOffset, zdrValueScale, zdrThresMin,
-                rCellMax, sign);
-
-        System.out.println("past findcells");
-
-        nCellsValid = BirdDensityProfile.analyzeCells(dbzImage, vradImage, texImage, clutterImage, cellImage,
-                dbznRang, dbznAzim, dbzElev, dbzScale, dbzOffset,
-                vradScale, vradOffset, clutterScale, clutterOffset,
-                texScale, texOffset, nCells, areaMin, cellDbzMin, cellStdDevMax,
-                cellClutterFraction, vradMinValue, dbzClutterMin, cmFlag, dualPolFlag, verbose);
-
-        System.out.println("past analyzecells");
-
-        BirdDensityProfile.fringeCells(cellImage, nRang, nAzim, azimuthScale, rangeScale, fringeDist);
-
-        System.out.println("past fringecells");
-
-        BirdDensityProfile.classify(dbznRang, dbznAzim, dbzRangeScale, dbzElev, dbzHeig, dbzScale, dbzOffset,
-                dbzAzimScale, dbzMissing, vradScale, vradOffset, vradMissing, rawReflMissing,
+        BirdDensityProfile.classify(dbznRang, dbznAzim, dbzRangeScale,
+                dbzElev, dbzHeig, dbzScale, dbzOffset,
+                dbzAzimScale, dbzMissing, vradScale, vradOffset, vradMissing,
+                rawReflMissing,
                 clutterScale, clutterOffset, cellImage, dbzImage, vradImage,
                 rawReflImage, clutterImage, zdata, rangeMin, rangeMax,
-                HLAYER, XOFFSET, XSCALE, XMEAN, height, azimMin, azimMax, vradMin, dbzClutter,
-                dbzMin, dBZx, DBZNOISE, NGAPMIN, NGAPBIN, NDBZMIN, layer, np, nPointsPtr,
-                nPointsAllPtr, nPointsClutterPtr, nPointsRainPtr, nPointsRainNoFringePtr,
+                HLAYER, XOFFSET, XSCALE, XMEAN, height, azimMin, azimMax, vradMin,
+                dbzClutter,
+                dbzMin, dBZx, DBZNOISE, NGAPMIN, NGAPBIN, NDBZMIN, layer, np,
+                nPointsPtr,
+                nPointsAllPtr, nPointsClutterPtr, nPointsRainPtr,
+                nPointsRainNoFringePtr,
                 clutterFlagInt, rawReflFlagInt, xflagInt);
-
-        System.out.println("past classify");
 
         // TODO finish this stuff right here
 
