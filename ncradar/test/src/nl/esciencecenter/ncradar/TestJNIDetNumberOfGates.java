42055c2852cc6af450e350d7281abfa02432e4ca
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-12-18 11:52:56 +0100

    added unit tests for native detNumberOfGates() method

---
+++ ncradar/test/src/nl/esciencecenter/ncradar/TestJNIDetNumberOfGates.java
@@ -0,0 +1,93 @@
+package nl.esciencecenter.ncradar;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class TestJNIDetNumberOfGates extends JNIMethodsVol2Bird {
+
+
+    @Test
+    public void testNativeDetNumberOfGates1() {
+
+
+        int nGatesActual;
+        int nGatesExpected;
+        int iLayer;
+        float layerThickness;
+        float rangeMin;
+        float rangeMax;
+        float rangeScale;
+        float elevAngle;
+        int nRang;
+        int nAzim;
+        float radarHeight;
+
+        
+        iLayer = 0;
+        layerThickness = 200.0f; // m
+        rangeMin = 10000.0f;     // m
+        rangeMax = 11000.0f;     // m
+        rangeScale = 500.0f;     // m
+        elevAngle = 0.0f;        // degrees
+        nRang = 520;
+        nAzim = 360;
+        radarHeight = 0.0f;      // m
+
+        nGatesActual = detNumberOfGates(iLayer, layerThickness,
+                                        rangeMin, rangeMax,
+                                        rangeScale, elevAngle,
+                                        nRang, nAzim,
+                                        radarHeight);
+
+        nGatesExpected = 2*360;
+
+        assertEquals(nGatesActual, nGatesExpected);
+
+    }
+
+
+    
+    @Test
+    public void testNativeDetNumberOfGates2() {
+
+
+        int nGatesActual;
+        int nGatesExpected;
+        int iLayer;
+        float layerThickness;
+        float rangeMin;
+        float rangeMax;
+        float rangeScale;
+        float elevAngle;
+        int nRang;
+        int nAzim;
+        float radarHeight;
+
+        
+        iLayer = 10;
+        layerThickness = 1000.0f; // m
+        rangeScale = (float) Math.sqrt(2.0)*100.0f;      // m
+        rangeMin = 100 * rangeScale;     // m
+        rangeMax = 110 * rangeScale;     // m
+
+        elevAngle = 45.0f;       // degrees
+        nRang = 520;
+        nAzim = 360;
+        radarHeight = 0.0f;      // m
+
+        nGatesActual = detNumberOfGates(iLayer, layerThickness,
+                                        rangeMin, rangeMax,
+                                        rangeScale, elevAngle,
+                                        nRang, nAzim,
+                                        radarHeight);
+
+        nGatesExpected = 10*360;
+
+        assertEquals(nGatesActual, nGatesExpected);
+
+    }
+
+    
+    
+}
