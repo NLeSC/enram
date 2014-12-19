8b136fecf3e1fa139861cf76a14a65416b4af7a0
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-12-19 11:59:41 +0100

    detNumberOfGates unit tests now work for an array of elevation angles, rather than just one

--- ncradar/test/src/nl/esciencecenter/ncradar/TestJNIDetNumberOfGates.java
+++ ncradar/test/src/nl/esciencecenter/ncradar/TestJNIDetNumberOfGates.java
@@ -18,25 +18,28 @@ public class TestJNIDetNumberOfGates extends JNIMethodsVol2Bird {
         float rangeMin;
         float rangeMax;
         float rangeScale;
-        float elevAngle;
+        float[] elevAngles;
+        int nElevAngles;
         int nRang;
         int nAzim;
         float radarHeight;
 
         
         iLayer = 0;
-        layerThickness = 200.0f; // m
-        rangeMin = 10000.0f;     // m
-        rangeMax = 11000.0f;     // m
-        rangeScale = 500.0f;     // m
-        elevAngle = 0.0f;        // degrees
+        layerThickness = 200.0f;       // m
+        rangeMin = 10000.0f;           // m
+        rangeMax = 11000.0f;           // m
+        rangeScale = 500.0f;           // m
+        elevAngles = new float[] {0.0f};    // degrees
+        nElevAngles = elevAngles.length;
+        
         nRang = 520;
         nAzim = 360;
         radarHeight = 0.0f;      // m
 
         nGatesActual = detNumberOfGates(iLayer, layerThickness,
                                         rangeMin, rangeMax,
-                                        rangeScale, elevAngle,
+                                        rangeScale, elevAngles, nElevAngles,
                                         nRang, nAzim,
                                         radarHeight);
 
@@ -59,7 +62,8 @@ public class TestJNIDetNumberOfGates extends JNIMethodsVol2Bird {
         float rangeMin;
         float rangeMax;
         float rangeScale;
-        float elevAngle;
+        float[] elevAngles;
+        int nElevAngles;
         int nRang;
         int nAzim;
         float radarHeight;
@@ -71,14 +75,15 @@ public class TestJNIDetNumberOfGates extends JNIMethodsVol2Bird {
         rangeMin = 100 * rangeScale;     // m
         rangeMax = 110 * rangeScale;     // m
 
-        elevAngle = 45.0f;       // degrees
+        elevAngles = new float[] {45.0f};       // degrees
+        nElevAngles = elevAngles.length;
         nRang = 520;
         nAzim = 360;
         radarHeight = 0.0f;      // m
 
         nGatesActual = detNumberOfGates(iLayer, layerThickness,
                                         rangeMin, rangeMax,
-                                        rangeScale, elevAngle,
+                                        rangeScale, elevAngles, nElevAngles,
                                         nRang, nAzim,
                                         radarHeight);
 
@@ -87,6 +92,50 @@ public class TestJNIDetNumberOfGates extends JNIMethodsVol2Bird {
         assertEquals(nGatesActual, nGatesExpected);
 
     }
+    
+    
+    
+    @Test
+    public void testNativeDetNumberOfGates3() {
+
+
+        int nGatesActual;
+        int nGatesExpected;
+        int iLayer;
+        float layerThickness;
+        float rangeMin;
+        float rangeMax;
+        float rangeScale;
+        float[] elevAngles;
+        int nElevAngles;
+        int nRang;
+        int nAzim;
+        float radarHeight;
+
+        
+        iLayer = 10;
+        layerThickness = 1000.0f;                        // m
+        rangeScale = (float) Math.sqrt(2.0)*100.0f;      // m
+        rangeMin = 100 * rangeScale;                     // m
+        rangeMax = 110 * rangeScale;                     // m
+
+        elevAngles = new float[] {45.0f,45.0f};           // degrees
+        nElevAngles = elevAngles.length;
+        nRang = 520;
+        nAzim = 360;
+        radarHeight = 0.0f;                              // m
+
+        nGatesActual = detNumberOfGates(iLayer, layerThickness,
+                                        rangeMin, rangeMax,
+                                        rangeScale, elevAngles, nElevAngles,
+                                        nRang, nAzim,
+                                        radarHeight);
+
+        nGatesExpected = nElevAngles * 10 * 360;
+
+        assertEquals(nGatesActual, nGatesExpected);
+
+    }    
 
     
     
