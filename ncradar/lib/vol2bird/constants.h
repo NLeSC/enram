179ea53512617413394e94b69b8db9a585273af0
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2015-02-24 14:22:12 +0100

    added the constants.h file that was previously part of the 'https://github.com/jspaaks/baltrad-node/' repo

---
+++ ncradar/lib/vol2bird/constants.h
@@ -0,0 +1,36 @@
+#define HLAYER                    (0.200 * 1000)
+#define NLAYER                    30
+#define NDATA                     4
+#define RANGMIN                   (5.0 * 1000)
+#define RANGMINSTDEV              5.0
+#define RANGMAXSTDEV              25.0
+#define RANGMAX                   ((25.0 + 5.0) * 1000)
+#define AZIMMIN                   0.0
+#define AZIMMAX                   360.0
+#define VRADMIN                   1.0
+#define NGAPBIN                   8
+#define NGAPMIN                   5
+#define NDBZMIN                   25
+#define VDIFMAX                   10.0
+#define VMASKMAX                  0.0
+#define EMASKMAX                  500.0
+#define DBZMIN                    -100.0
+#define DBZMAX                    20.0
+#define DBZNOISE                  -40.0
+#define DBZRAIN                   0.0
+#define DBZCELL                   15.0
+#define STDEVCELL                 5.0
+#define AREACELL                  4
+#define CLUTPERCCELL              0.5
+#define NEIGHBOURS                5
+#define STDEVSCALE                0.1
+#define NTEXBINAZIM               3
+#define NTEXBINRANG               3
+#define NTEXMIN                   4
+#define DBZCLUTTER                -10.0
+#define DBZFACTOR                 360.7
+#define SIGMABIRD                 11.0
+#define STDEVBIRD                 2.0
+#define NBINSGAP                  8
+#define NOBSGAPMIN                5
+#define COMPLEX_REFRACTIVE_INDEX  0.964
